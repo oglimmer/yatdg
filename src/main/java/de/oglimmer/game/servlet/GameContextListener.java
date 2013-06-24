97d2a315754af2624ee3177e701c593b8cb6e447
Oli Zimpasser <oglimmer@gmail.com>
2013-06-24 21:35:53 +0200

    Removed Jetty inside Tomcat

--- src/main/java/de/oglimmer/game/servlet/GameContextListener.java
+++ src/main/java/de/oglimmer/game/servlet/GameContextListener.java
@@ -3,11 +3,6 @@ package de.oglimmer.game.servlet;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-
 import de.oglimmer.game.logic.GameManager;
 
 // import javax.servlet.annotation.WebListener;
@@ -19,8 +14,6 @@ import de.oglimmer.game.logic.GameManager;
 // @WebListener
 public class GameContextListener implements ServletContextListener {
 
-	private static final Log log = LogFactory.getLog(GameContextListener.class);
-
 	/**
 	 * Default constructor.
 	 */
@@ -29,19 +22,6 @@ public class GameContextListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		try {
-			Server server = new Server(9999);
-
-			WebAppContext webapp = new WebAppContext();
-			webapp.setContextPath("/");
-			webapp.setWar(System.getProperty("yatdg_path"));
-			server.setHandler(webapp);
-
-			server.start();
-			// server.join();
-		} catch (Exception e) {
-			log.error("Failed to start Jetty", e);
-		}
 	}
 
 	@Override
